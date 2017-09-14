3de4b5bf698a6145880d4174e79b0f35ba1e23dc
Mohamed Ahmed <error17191@gmail.com>
2017-09-14 14:51:54 +0300

    Sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,16 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: /\.s[ac]ss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
     },
     plugins: []
 
